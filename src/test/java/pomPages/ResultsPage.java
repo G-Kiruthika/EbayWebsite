d6eeccff885106bdde9a2f93058862a4bac9430e
G-Kiruthika <88583981+G-Kiruthika@users.noreply.github.com>
2025-08-25 19:00:47 +0530

    commit3

--- src/test/java/pomPages/ResultsPage.java
+++ src/test/java/pomPages/ResultsPage.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package pomPages;
 
 import org.openqa.selenium.By;
@@ -18,7 +17,6 @@ public class ResultsPage {
 
     private By searchBox = By.id("gh-ac"); // eBay search box
     private By searchButton = By.cssSelector("div.gh-search-button__wrap button"); // Search button
-    //private By productLinks = By.xpath("//ul[@class='srp-results srp-grid clearfix']//li[1]//a"); // Product results list
     private By productLinks = By.cssSelector("ul.srp-results li.s-item a.s-item__link");
     
     public ResultsPage(WebDriver driver) {
@@ -59,42 +57,3 @@ public class ResultsPage {
     }
 
 }
-=======
-package pomPages;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.List;
-
-public class ResultsPage {
-    WebDriver driver;
-    WebDriverWait wait;
-
-    private By searchBox = By.id("gh-ac"); // eBay search box
-    private By searchButton = By.cssSelector("div.gh-search-button__wrap button"); // Search button
-    private By productLinks = By.cssSelector("ul.srp-results li.s-item a.s-item__link"); // Product results list
-
-    public ResultsPage(WebDriver driver) {
-        this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-    }
-
-    public void searchProduct(String productName) {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(searchBox)).clear();
-        driver.findElement(searchBox).sendKeys(productName);
-        driver.findElement(searchButton).click();
-    }
-
-    public void clickFirstProduct() {
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(productLinks));
-        driver.findElement(productLinks).click();
-         // click first product
-        System.out.println("Product Clicked");
-    }
-}
->>>>>>> 3b46b920e222fa0cb7bf3ec787595df0614d0322
--- src/test/java/pomTest/testscripts.java
+++ src/test/java/pomTest/testscripts.java
@@ -27,7 +27,7 @@ import pomPages.Login;
 import pomPages.Search;
 import pomPages.AddToCart;
 import pomPages.ResultsPage;
-public class testscript{
+public class testscripts{
 	public WebDriver driver;
 	
 	@BeforeClass
@@ -72,30 +72,20 @@ public class testscript{
 	public void addToCartProduct()throws Exception{
 		AddToCart add = new AddToCart(driver);
 		ResultsPage result = new ResultsPage(driver);
-<<<<<<< HEAD:src/test/java/pomTest/testscript.java
 		Thread.sleep(2000);
-=======
-		result.searchProduct("watch");
->>>>>>> 3b46b920e222fa0cb7bf3ec787595df0614d0322:src/test/java/pomTest/testscripts.java
+
 		result.clickFirstProduct();
 		// switch to new tab
         ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(1));
-<<<<<<< HEAD:src/test/java/pomTest/testscript.java
+
         Thread.sleep(3000);
         add.clickAddToCart();
         add.clickSeeInCart();
         //add.clickClose();
-        Thread.sleep(3000);
-        add.goToCart();
         Thread.sleep(2000);
         add.clickSignOut();
-	
-=======
-        add.clickAddToCart();
-        add.clickClose();
-        add.goToCart();
->>>>>>> 3b46b920e222fa0cb7bf3ec787595df0614d0322:src/test/java/pomTest/testscripts.java
+        Thread.sleep(3000);
 	}
 	
 	 @AfterClass
